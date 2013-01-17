041191ed083f0031861cb6e27f7ff893bcbf002f
Manuele Menozzi <manuele.menozzi@gmail.com>
2013-01-17 14:57:58 +0100

    Sposta phpunit.xml in radice e rimuove bootstrap.php inutile

--- Tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'SplClassLoader.php');
-
-$classLoader = new SplClassLoader(null, realpath(__DIR__ . DIRECTORY_SEPARATOR . '..'));
-$classLoader->register();
